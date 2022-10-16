c0b4c8f823401ce082d8616f6748b24f83d83559
Duy Phong <nguyenduyphong1504@gmail.com>
2022-10-16 12:15:07 +0700

    delete comet chat

--- src/pages/CometChat/CometChat.jsx
+++ src/pages/CometChat/CometChat.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { CometChatUI } from "../../cometchat-pro-react-ui-kit/CometChatWorkspace/src";
-// import { CometChatUI } from "../../cometchat-pro-react-ui-kit/";
 const CometChat = () => {
   return (
     <div style={{ width: "98vw", height: "95vh", backgroundColor: "white" }}>
