6d82d976d999cf88d3fe8309faf6f1720f3bca22
Duy Phong <nguyenduyphong1504@gmail.com>
2022-12-02 18:11:56 +0700

    update server back-end

--- src/constants/constans.js
+++ src/constants/constans.js
@@ -1,4 +1,4 @@
 export const APP_ID = "22482503e22d9217";
 export const APP_REGION = "us";
 export const AUTH_KEY = "19fd903aca95a6a5d5d4cc3baa45e48c04172840";
-export const SERVER = "https://social-media-api-moongo.herokuapp.com";
+export const SERVER = "https://omf.onrender.com";
